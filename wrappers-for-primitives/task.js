2859e570eca31049dfa1f3b4e2d809ac1a56238d
ANesbytnov <nesbytnovae@mail.ru>
2019-03-15 22:23:35 +0300

    Lesson 4 completed

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,4 +1,14 @@
+/*jshint esversion: 6*/
+'use strict';
+
 function calculateMortgage() {
+	// debug
+	// window.percent.value = 10;
+	// window.contribution.value = 1000;
+	// window.amount.value = 20000;
+	// window.date.value = '2021-01-11';
+	//
+
     let percent = window.percent.value;
     let contribution = window.contribution.value;
     let amount = window.amount.value;
@@ -9,10 +19,32 @@ function calculateMortgage() {
     span.textContent = result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+function diff_months(dt2, dt1) {
+  let diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= (60 * 60 * 24 * 7 * 4);
+  return Math.abs(Math.round(diff));
+}
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+const checkNum = (name, value) => Number.isNaN(parseInt(value)) ? `Параметр "${name}" содержит неправильное значение "${value}". ` : parseInt(value);
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+	const numPercent = checkNum('Процентная ставка', percent);
+	if (typeof(numPercent) === 'string') {
+		return numPercent;
+	}
+	const numContribution = checkNum('Начальный взнос', contribution);
+	if (typeof(numContribution) === 'string') {
+		return numContribution;
+	}
+	const numAmount = checkNum('Общая стоимость', amount);
+	if (typeof(numAmount) === 'string') {
+		return numAmount;
+	}
+	const n = diff_months(new Date(date), new Date());
+
+	const P = percent / 100 / 12;
+	const totalSum = n * (amount - contribution) * (P + P / (( Math.pow(1 + P, n) - 1)));
+	return totalSum.toFixed(2);
 }
 
 function sayHello() {
@@ -20,9 +52,7 @@ function sayHello() {
     let greeting = getGreeting(name);
     let span = window.helloResult;
     span.textContent = greeting;
+    console.log(greeting);
 }
 
-function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
-}
+const getGreeting = (name) => `Привет, мир! Меня зовут ${name.trim() ? name.trim() : 'Аноним'}.`;
